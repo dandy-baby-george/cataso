c68ffe75087033d98039d5a8c9c3f24eb30fa499
Your Name <you@example.com>
2025-02-09 22:53:45 +0900

    typescriptまわり修正

--- backend/src/interfaces/routes/api.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { DiceController } from "../controllers/DiceController";
-
-const router = Router();
-
-router.post("/roll-dice", DiceController.rollDice);
-
-export default router;
--- backend/src/interfaces/server.ts
+++ backend/src/interfaces/server.ts
@@ -1,13 +1,20 @@
 require('dotenv').config({ path: '../.env' });
+// import { DiceController } from './controllers/diceController.ts';
 
 const express = require('express');
 const app = express();
 
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// const diceController = new DiceController();
+
+app.get('/', () => {
+  Response.json({ text: 'Hello, World!' });
 });
+
+// app.get('/api/dice', (req, res) => {
+//   return diceController.roll(req, res);
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
