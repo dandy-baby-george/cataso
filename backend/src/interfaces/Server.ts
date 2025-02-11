df468a645e9c212dcfa00b947758b2a36ca0a98e
Your Name <you@example.com>
2025-02-11 23:25:14 +0900

    サイコロAPI実装&コンパイル自動化

--- backend/src/interfaces/Server.ts
+++ backend/src/interfaces/Server.ts
@@ -1,43 +1,24 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 import { DiceController } from './controllers/DiceController';
 
+dotenv.config();
+const port = process.env.PORT;
 const app = express();
-const env = process.env;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const port = process.env.PORT;
-const port = env.PORT;
-
 app.get('/', (req: Request, res: Response) => {
-    res.send('hello world');
+  res.send('hello world!');
 });
 
-app.get('/api/dice', (req, res) => {
-    const diceController = new DiceController();
+app.get('/api/dice', (req: Request, res: Response) => {
+  const diceController = new DiceController();
 
-    return diceController.roll(req, res);
-});
-
-// エラーハンドリングミドルウェア
-app.use((err: any, req: Request, res: Response) => {
-    // 開発環境の場合、エラーメッセージとスタックトレースを表示
-    if (env.NODE_ENV === 'development') {
-        res.status(500).json({
-            message: err.message,
-            stack: err.stack,
-        });
-    } else {
-        // 本番環境の場合、一般的なエラーメッセージを表示
-        res.status(500).json({
-            message: 'Something went wrong! Please try again later.',
-        });
-    }
-
-    console.error(err.stack); // サーバーログにスタックトレースを記録
+  return diceController.roll(req, res);
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
