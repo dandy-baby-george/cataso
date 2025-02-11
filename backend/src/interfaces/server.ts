5dd653b69e8201ae67b4f8a6b8beba5d3b407407
Your Name <you@example.com>
2025-02-11 13:19:11 +0900

    hello world表示設定

--- backend/src/interfaces/server.ts
+++ backend/src/interfaces/server.ts
@@ -1,21 +1,22 @@
-require('dotenv').config({ path: '../.env' });
+// require('dotenv').config({ path: '../.env' });
 // import { DiceController } from './controllers/diceController.ts';
 
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT;
+// const port = process.env.PORT;
+const port = 3000;
 
 // const diceController = new DiceController();
 
-app.get('/', () => {
-  Response.json({ text: 'Hello, World!!' });
-});
+app.get("/", (req, res) => {
+  res.send("hello world");
+})
 
 // app.get('/api/dice', (req, res) => {
 //   return diceController.roll(req, res);
 // });
 
-app.listen(port, () => {
+app.listen(port, (req, res) => {
   console.log(`Server is running on port ${port}`);
 });
