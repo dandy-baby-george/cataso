cba938259be6bd949a786721ee85bbd3a0fefcbb
Your Name <you@example.com>
2025-02-11 18:19:42 +0900

    コンパイル時のバグ修正

--- backend/src/interfaces/Server.ts
+++ backend/src/interfaces/Server.ts
@@ -1,22 +1,26 @@
 // require('dotenv').config({ path: '../.env' });
 // import { DiceController } from './controllers/diceController.ts';
 
-const express = require('express');
+import express from 'express';
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // const port = process.env.PORT;
 const port = 3000;
 
 // const diceController = new DiceController();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-})
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
 
 // app.get('/api/dice', (req, res) => {
 //   return diceController.roll(req, res);
 // });
 
-app.listen(port, (req, res) => {
-  console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
