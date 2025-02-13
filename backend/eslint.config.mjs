b123c7fc781e3d10b7cc1a0505d521308e15dce4
Your Name <you@example.com>
2025-02-13 22:20:48 +0900

    a

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -10,22 +10,15 @@ export default [
       parser: typescriptParser,
     },
     rules: {
-      'no-unused-vars': ['error'], // 未使用の変数をエラーとして検出
       'no-undef': ['error'], // 未定義の変数をエラーとして検出
       eqeqeq: ['error', 'always'], // 厳密な等価演算子を強制
       'no-console': ['warn'], // console.log の使用を警告
-      indent: ['error', 4], // インデントを4スペースで強制
       quotes: ['error', 'single'], // シングルクォートを強制
       semi: ['error', 'always'], // セミコロンを必須に
-      'brace-style': ['error', '1tbs'], // ブレースのスタイルを "1tbs" に強制
       camelcase: ['error', { properties: 'always' }], // キャメルケースを強制
-      'newline-after-var': ['error', 'always'], // 変数宣言後の空行を強制
       'no-magic-numbers': ['warn', { ignore: [0, 1] }], // マジックナンバーの使用を警告
-      'consistent-return': ['error'], // 一貫した return を強制
       'no-var': ['error'], // var の使用を禁止
-      complexity: ['warn', { max: 10 }], // 関数の複雑さを制限
       'prefer-const': ['error'], // 再代入されない変数に const を推奨
-      // "import/no-unresolved": ["error"], // モジュールの解決をチェック
       'padding-line-between-statements': [
         'error',
         { blankLine: 'always', prev: '*', next: 'return' },
