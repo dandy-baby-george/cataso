893feac522093cee21a37e57dc673fc08c149283
Your Name <you@example.com>
2025-02-16 21:17:36 +0900

    サイコロAPIの最大数設定

--- backend/src/interfaces/controllers/DiceController.ts
+++ backend/src/interfaces/controllers/DiceController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import { Dice } from '../../domain/valueObjects/dice/Dice';
 
 export class DiceController {
+  private static MAX_DICE_COUNT: number = 9;
+
   public roll(req: Request, res: Response): void {
     const quantityParam = req.query.quantity;
 
@@ -9,10 +11,11 @@ export class DiceController {
       typeof quantityParam !== 'string' ||
       !quantityParam ||
       !/^\d+$/.test(quantityParam) ||
-      parseInt(quantityParam) < 1
+      parseInt(quantityParam) < 1 ||
+      parseInt(quantityParam) > DiceController.MAX_DICE_COUNT
     ) {
       res.status(400).json({
-        error: `Invalid quantity parameter. Must be a number.${
+        error: `Invalid quantity parameter. Must be a number smaller than 10.${
           req.query.quantity ? ` value: ${req.query.quantity}` : ''
         }`,
       });
